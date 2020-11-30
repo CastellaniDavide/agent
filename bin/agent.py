e19b2cb537088b23383db4676f98161964f6a74c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-30 08:44:56 +0100

    Created bin/agent.py

---
+++ bin/agent.py
@@ -0,0 +1,19 @@
+"""agent
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-30"
+
+class agent:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(agent.agent())
+	
+	def agent():
+		"""agent first funtion
+		"""
+		return "agent" #for the test (see test_agent.py)
+		
+if __name__ == "__main__":
+	agent()
